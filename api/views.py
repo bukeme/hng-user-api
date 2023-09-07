cbe9610b81429932ec4f6370af3498e0f6c1183f
Ukeme Bassey <basseyukemme@gmail.com>
2023-09-07 12:01:38 +0100

    pushing project

---
+++ api/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+from datetime import datetime
+import pytz
+
+
+def user_data(request):
+	slack_name = request.GET.get('slack_name', '')
+	track = request.GET.get('track', '')
+	d = datetime.now()
+	utc_time = datetime.now(pytz.utc)
+	data = {
+		'slack_name': slack_name,
+		'current_day': d.strftime("%A"),
+		'utc_time': utc_time,
+		'track': track,
+		'github_file_url': '',
+		'github_repo_url': '',
+		'status_code': 200,
+	}
+	return JsonResponse(data, status=200)
